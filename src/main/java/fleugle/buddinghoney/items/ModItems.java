328bd29a61c872cbfc2e13cafb1dcbfe94412541
Nikita-laptop <astraimer.ru@gmail.com>
2024-08-19 11:24:48 +0200

    1.1

--- src/main/java/fleugle/buddinghoney/items/ModItems.java
+++ src/main/java/fleugle/buddinghoney/items/ModItems.java
@@ -7,9 +7,15 @@ import fleugle.buddinghoney.utility.ModToolMaterial;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SmithingTemplateItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.List;
 
 public class ModItems {
 
@@ -20,10 +26,30 @@ public class ModItems {
 
     public static final Item COGSWORD = registerItem("cogsword", new CogswordItem(ModToolMaterial.BEESTANITE,2.6f, -2.4f,new Item.Settings(), false));
 
-    public static final Item AMETHYST_COGSWORD = registerItem("amethyst_cogsword", new CogswordItem(ModToolMaterial.BEESTANITE,2.6f, -2.4f,new Item.Settings(), true));
+    public static final Item AMETHYST_COGSWORD = registerItem("amethyst_cogsword.json", new CogswordItem(ModToolMaterial.BEESTANITE,2.6f, -2.4f,new Item.Settings(), true));
 
     public static final Item HONEYCOMB_SHOTGUN = registerItem("honeycomb_shotgun", new HoneycombShotgunItem(new Item.Settings(), false));
 
+
+    public static final Item AMETHYST_UPGRADE_SMITHING_TEMPLATE = registerItem("amethyst_upgrade_smithing_template", new SmithingTemplateItem(
+            Text.translatable(
+                    Util.createTranslationKey("item", Buddinghoney.id("smithing_template.amethyst_upgrade.applies_to"))
+            ).formatted(Formatting.BLUE),
+            Text.translatable(
+                    Util.createTranslationKey("item", Buddinghoney.id("smithing_template.amethyst_upgrade.ingredients"))
+            ).formatted(Formatting.BLUE),
+            Text.translatable(
+                    Util.createTranslationKey("upgrade", Buddinghoney.id("amethyst_upgrade"))
+            ).formatted(Formatting.GRAY),
+            Text.translatable(
+                    Util.createTranslationKey("item", Buddinghoney.id("smithing_template.amethyst_upgrade.base_slot_description"))
+            ),
+            Text.translatable(
+                    Util.createTranslationKey("item", Buddinghoney.id("smithing_template.amethyst_upgrade.additions_slot_description"))
+            ),
+            List.of(new Identifier("item/empty_slot_sword")),
+            List.of(new Identifier("item/empty_slot_amethyst_shard"))
+    ));
 
 
 
@@ -44,6 +70,8 @@ public class ModItems {
 
             entries.add(BEESTANITE);
 
+            entries.add(AMETHYST_UPGRADE_SMITHING_TEMPLATE);
+
         });
 
         //FOOD_AND_DRINKS TAB ITEMS REGISTRY
@@ -61,6 +89,8 @@ public class ModItems {
             //weapons
             entries.add(COGSWORD);
             entries.add(AMETHYST_COGSWORD);
+
+            entries.add(HONEYCOMB_SHOTGUN);
 
 
             //utility items
